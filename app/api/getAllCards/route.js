b72bed4f932b6e95cdfcaf9a2f2e9fb5aa865a18
sahasuvankar7 <suvankarsaha084@gmail.com>
2024-08-13 02:02:33 +0530

    small-fix

--- app/api/getAllCards/route.js
+++ app/api/getAllCards/route.js
@@ -1,15 +1,19 @@
+export const dynamic = "force-dynamic";
 import prisma from "@/lib/db";
 import { NextResponse } from "next/server";
 // get all cards
 export async function GET() {
   try {
     const question = await prisma.question.findMany({});
-    if(!question){
-        return NextResponse.json({message:'question not found'},{status:404});
+    if (!question) {
+      return NextResponse.json(
+        { message: "question not found" },
+        { status: 404 }
+      );
     }
     return NextResponse.json(question);
   } catch (err) {
     console.log(err);
-    return NextResponse.json({message:err})
+    return NextResponse.json({ message: err });
   }
 }
