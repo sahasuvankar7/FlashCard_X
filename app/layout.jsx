e0672584b99ec7de20dce382992ba2111f7b3b03
sahasuvankar7 <suvankarsaha084@gmail.com>
2024-08-13 17:31:06 +0530

    small change

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Link from "next/link";
-import Image from "next/image";
 import Navbar from "@/components/navbar";
+export const runtime = "edge" // optional
 
 const inter = Inter({ subsets: ["latin"] });
 
